a339778140296a608cb8d6f0bb55a8ede24afd97
Marcus <gullbeerg@gmail.com>
2018-09-30 20:48:13 +0200

    Scaffhold for notes module

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,4 +2,20 @@ console.log('Starting App');
 
 const fs = require('fs');
 const _ =  require('lodash');
-const notes = require('./notes')
+const yargs = require('yargs');
+const notes = require('./notes');
+
+const argv = yargs.argv;
+let command = process.argv[2]
+
+if (command === 'add') {
+    notes.add(argv.title, argv.body);
+} else if (command === 'list') {
+    notes.all();
+} else if (command === 'read') {
+    notes.read(argv.title);
+} else if (command === 'remove') {
+    notes.remove(argv.title);
+} else {
+    console.log('Command not recognized');
+}
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,11 +1,26 @@
-console.log('Starting notes.js');
+let add = (title, body) => {
+
+}
+
+let remove = () => {
+
+}
+
+let all = () => {
+
+}
+
+let read = (title) => {
+
+}
 
 /**
  * Module is an object that every node file has and the export property
  * is what will be added to the variable when we require.
  */
 module.exports = {
-    addNote: () => {
-
-    }
-}
+    add,
+    remove,
+    all,
+    read
+};
